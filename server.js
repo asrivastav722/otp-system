04b9f46f369791ac55450a5ca9df91f1b241ba85
Akash Raj <asrivastav722@gmail.com>
2026-03-13 13:45:55 +0530

    commit

--- server.js
+++ server.js
@@ -3,30 +3,44 @@ import { addJob, queueSize } from "./queue/smsQueue.js";
 import apiKeyAuth from "./middleware/apiKeyAuth.js";
 
 const app = express();
+
+// Render provides PORT automatically
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
-app.post("/api/sms/send", apiKeyAuth, (req, res) => {
-
-  const { to, message } = req.body;
-
-  if (!to || !message) {
-    return res.status(400).json({ error: "Missing fields" });
-  }
-
-  addJob({
-    to,
-    message,
-    createdAt: Date.now()
-  });
-
-  res.json({
-    success: true,
-    status: "queued",
-    queueSize: queueSize()
-  });
-
+app.get("/", (req, res) => {
+  res.send("SMS API Running");
 });
 
-app.listen(3000, () => {
-  console.log("SMS API running on port 3000");
+app.post("/api/sms/send", apiKeyAuth, async (req, res) => {
+  try {
+    const { to, message } = req.body;
+
+    if (!to || !message) {
+      return res.status(400).json({ error: "Missing fields" });
+    }
+
+    await addJob({
+      to,
+      message,
+      createdAt: Date.now()
+    });
+
+    res.json({
+      success: true,
+      status: "queued",
+      queueSize: queueSize()
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: "Server error",
+      message: err.message
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`SMS API running on port ${PORT}`);
 });
